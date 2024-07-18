c40a6bc79152567163e1ec23e0e204a0f52196e5
vivek <vivek.v78@gmail.com>
2024-07-18 10:41:34 +0100

    Added shortcut for double click F4 and disabled unwanted animationcs

--- keyboards/keychron/k8_pro/iso/rgb/keymaps/custom/keymap.c
+++ keyboards/keychron/k8_pro/iso/rgb/keymaps/custom/keymap.c
@@ -29,7 +29,8 @@ enum td_keycodes {
     TD_E,
     TD_R,
     TD_N,
-    TD_T
+    TD_T,
+    TD_F4
 };
 
 // clang-format off
@@ -58,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 
 [WIN_BASE] = LAYOUT_tkl_iso(
-     KC_ESC,   KC_F1,       KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,      KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,             KC_PSCR,   KC_CTANA, RGB_MOD,
+     KC_ESC,   KC_F1,       KC_F2,    KC_F3,    TD(TD_F4),KC_F5,    KC_F6,      KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,             KC_PSCR,   KC_CTANA, RGB_MOD,
      KC_GRV,   KC_1,        KC_2,     KC_3,     KC_4,     KC_5,     KC_6,       KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,    KC_HOME,  KC_PGUP,
      KC_TAB,   KC_Q,        KC_W,     TD(TD_E), TD(TD_R), TD(TD_T), KC_Y,       KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,            KC_DEL,    KC_END,   KC_PGDN,
      KC_CAPS,  TD(TD_A),    KC_S,     KC_D,     TD(TD_F), KC_G,     KC_H,       KC_J,     KC_K,     TD(TD_L), KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,
@@ -307,12 +308,13 @@ tap_dance_action_t tap_dance_actions[] = {
     [TD_R] = ACTION_TAP_DANCE_TAP_AND_HOLD(KC_R,0,0,KC_R,KC_LCTL),
     [TD_X] = ACTION_TAP_DANCE_DOUBLE(KC_X,LCTL(KC_X)),
     [TD_Z] = ACTION_TAP_DANCE_DOUBLE(KC_Z,LCTL(KC_Z)),
+    [TD_F4] = ACTION_TAP_DANCE_DOUBLE(KC_F4,LCTL(KC_F4)),
     [TD_T] = ACTION_TD_DOUBLE_TAP_AND_HOLD(KC_T,0,LCA(KC_T)),
 //    [TD_X] = ACTION_TAP_DANCE_TAP_AND_HOLD(KC_X,KC_X,KC_LCTL,0,0),
 //    [TD_Z] = ACTION_TAP_DANCE_TAP_AND_HOLD(KC_Z,KC_Z,KC_LCTL,0,0),
     // Under development
-//    [TD_V] = ACTION_TD_DOUBLE_TAP_AND_HOLD(KC_V,RCS(KC_V),LSFT(KC_INS)),
-    [TD_V] = ACTION_TAP_DANCE_TAP_AND_HOLD(KC_V,KC_V,KC_LCTL,KC_INS,KC_LSFT),
+    [TD_V] = ACTION_TD_DOUBLE_TAP_AND_HOLD(KC_V,RCS(KC_V),LSFT(KC_INS)),
+//    [TD_V] = ACTION_TAP_DANCE_TAP_AND_HOLD(KC_V,KC_V,KC_LCTL,KC_INS,KC_LSFT),
     [TD_A] = ACTION_TAP_DANCE_TAP_AND_HOLD(KC_A,0,0,KC_A,KC_LCTL),
     [TD_B] = ACTION_TAP_DANCE_TAP_AND_HOLD(KC_B,0,0,KC_B,KC_LCTL),
     [TD_F] = ACTION_TAP_DANCE_TAP_AND_HOLD(KC_F,0,0,KC_F,KC_LCTL),
